f170d46e97c45cb4df94907e405b0970bb4fbf0c
xsj <652539812@qq.com>
2022-04-23 17:18:10 +0800

    second commit

--- crm/src/main/java/com/sangjie/exception/ConvertException.java
+++ crm/src/main/java/com/sangjie/exception/ConvertException.java
@@ -7,4 +7,8 @@ public class ConvertException extends Exception{
     public ConvertException(String message) {
         super(message);
     }
+
+    public ConvertException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
