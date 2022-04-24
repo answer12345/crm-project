44122b35b657f7a3c94ef12173ef581e8f7431f8
answer12345 <69234185+answer12345@users.noreply.github.com>
2022-04-24 14:29:15 +0800

    second change

--- crm/src/main/java/com/sangjie/exception/ConvertException.java
+++ crm/src/main/java/com/sangjie/exception/ConvertException.java
@@ -7,8 +7,4 @@ public class ConvertException extends Exception{
     public ConvertException(String message) {
         super(message);
     }
-
-    public ConvertException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
